9c6bbec7dc0fe46b9f23fab1866ff884f21d1653
bspence205 <brianspencer@spencerbrian.com>
2022-01-17 23:41:52 -0800

    created  gameChoices array to hold choice values, created user/cpu scores

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 // function for computer choice to randomly be generated
 
+const gameChoices = ["scissors", "paper", "rock"];
+const userScore = 0;
+const cpuScore = 0;
+
 function computerPlay() {
   let cpuChoice = Math.floor(Math.random() * 3);
   if (cpuChoice == 0) {
